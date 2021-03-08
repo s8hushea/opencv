f1fc96d0717ba960313e51cadbca16d22e22654e
Hus <s8hushea@stud.uni-saarland.de>
2021-03-08 17:54:56 +0100

    usecase

--- MiniUtils.py
+++ MiniUtils.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import pyk4a
 
-def getContours(img, depth, cThr= [222,77], showCanny = False, minArea = 1000, filter = 0, draw = False, maxArea = 30000):
+def getContours(img, depth, cThr= [222,77], showCanny = False, minArea = 200, filter = 0, draw = False, maxArea = 30000):
     '''l_h = cv2.getTrackbarPos("L-H", "Trackbars")
     l_s = cv2.getTrackbarPos("L-S", "Trackbars")
     l_v = cv2.getTrackbarPos("L-V", "Trackbars")
@@ -10,46 +10,36 @@ def getContours(img, depth, cThr= [222,77], showCanny = False, minArea = 1000, f
     u_s = cv2.getTrackbarPos("U-S", "Trackbars")
     u_v = cv2.getTrackbarPos("U-V", "Trackbars")'''
     #imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-    lower_hsv = np.array([0, 0, 140])
-    upper_hsv = np.array([115, 169, 255])
-    #lower_red = np.array([160, 20, 70])
-    #upper_red = np.array([190, 255, 255])
-    #lower_blue = np.array([101, 50, 38])
-    #upper_blue = np.array([110, 255, 255])
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #lower_hsv = np.array([l_h, l_s, l_v])
+    #upper_hsv = np.array([u_h, u_s, u_v])
+    lower_hsv = np.array([0, 155, 0])
+    upper_hsv = np.array([255, 255, 255])
     mask = cv2.inRange(imgHSV, lower_hsv, upper_hsv)
-    #mask1 = cv2.inRange(imgHSV, lower_red, upper_red)
-    #mask2 = cv2.inRange(imgHSV, lower_blue, upper_blue)
-    #imgBlur = cv2.GaussianBlur(imgHSV, (5, 5), 1)
-    #imgCanny = cv2.Canny(imgBlur, cThr[0], cThr[1])
-
-    #imgCanny = cv2.Canny(imgBlur, 136, 68)
     kernel = np.ones((5, 5))
     mask = cv2.erode(mask, kernel)
     #cv2.imshow('mask',mask)
-    mask = cv2.bitwise_not(mask)
-    #mask1 = cv2.erode(mask1, kernel)
-    #mask2 = cv2.erode(mask2, kernel)
-    #mask = cv2.bitwise_or(mask1, mask2)
-    #target = cv2.bitwise_and(img, img, mask=mask)
+    #mask = cv2.bitwise_not(mask)
 
-    #imgDial = cv2.dilate(imgCanny, kernel, iterations=3)
-    #imgThre = cv2.erode(imgDial, kernel, iterations=2)
-    ###cv2.imshow('hsv', imgHSV)
+    cv2.imshow('hsv', imgHSV)
     if showCanny:
         cv2.imshow('Canny', mask)
 
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     finalContours = []
-
+    approxes = []
     for i in contours:
         area = cv2.contourArea(i)
-        if minArea < area < 5000:
+        #approxes.append(area)
+        if minArea < area < maxArea:
             approx = cv2.approxPolyDP(i, 0.02*cv2.arcLength(i, True), True)
+            #approxes.append(approx)
             bbox = cv2.boundingRect(approx)
             if filter > 0:
-                if len(approx) == filter or len(approx) == 6 or len(approx) == 8:
-                    finalContours.append([len(approx), area, approx, bbox, i])
+                if len(approx) == filter or len(approx) == filter + 1 or len(approx) == filter - 1 or len(approx) == 6 or len(approx) == 8:
+                    if 900 < i[2][0][0]:
+                        finalContours.append([len(approx), area, approx, bbox, i])
+
             else:
                 finalContours.append([len(approx), area, approx, bbox, i])
 
@@ -61,6 +51,16 @@ def getContours(img, depth, cThr= [222,77], showCanny = False, minArea = 1000, f
     return img, finalContours
 
 
+def midPoints(conts):
+    midptslist = []
+    for obj in conts:
+        top_left_x = obj[3][0]
+        top_left_y = obj[3][1]
+        buttom_right_x = top_left_x + obj[3][2]
+        buttom_right_y = top_left_y + obj[3][3]
+        midP = [(top_left_x + buttom_right_x)/2, (top_left_y + buttom_right_y)/2]
+        midptslist.append(midP)
+    return midptslist
 def reorder(myPoints):
 
     myPointsNew = np.zeros_like(myPoints)
--- maisub.py
+++ maisub.py
@@ -3,41 +3,86 @@ import datetime
 import cv2
 import numpy as np
 import json
-
+import MiniUtils
 import pyk4a
 from helpers import colorize
 from pyk4a import Config, PyK4A
 
 
+def nothing(x):
+    # any operation
+    pass
+
+
 def main():
-    k4a = PyK4A(
-        Config(
-            color_resolution=pyk4a.ColorResolution.RES_720P,
-            color_format=pyk4a.ImageFormat.COLOR_BGRA32,
-            depth_mode=pyk4a.DepthMode.NFOV_UNBINNED,
-            synchronized_images_only=True,
+    '''cv2.namedWindow("Trackbars")
 
-        )
-    )
+    cv2.createTrackbar("L-H", "Trackbars", 0, 255, nothing)
+    cv2.createTrackbar("L-S", "Trackbars", 0, 255, nothing)
+    cv2.createTrackbar("L-V", "Trackbars", 0, 255, nothing)
+    cv2.createTrackbar("U-H", "Trackbars", 0, 255, nothing)
+    cv2.createTrackbar("U-S", "Trackbars", 0, 255, nothing)
+    cv2.createTrackbar("U-V", "Trackbars", 0, 255, nothing)'''
 
-    k4a.start()
-    # getters and setters directly get and set on device
-    k4a.whitebalance = 4500
-    assert k4a.whitebalance == 4500
-    k4a.whitebalance = 4510
-    assert k4a.whitebalance == 4510
+    wP = 210
+    hP = 297
+    scale = 3
 
+    img = cv2.imread('colorimg.bmp')
+    depth = cv2.imread('depth.bmp')
+    #cv2.rectangle(img, (489, 269), (612, 344), (0, 255, 0), 2)
+    slot0 = [[489, 269], [612, 269], [489, 344], [612, 344]] #slot0
+    slot0_mP = [550.5, 306.5]
+    #cv2.rectangle(img, (645, 273), (780, 350), (0, 255, 0), 2)
+    slot1 = [[645, 273], [780, 273], [645, 350], [780, 350]]#slot1
+    slot1_mP = [712.5, 311.5]
+    #cv2.rectangle(img, (805, 271), (920, 346), (0, 255, 0), 2)
+    slot2= [[805, 271], [920, 271], [805, 346], [920, 346]]#slot2
+    slot2_mP = [862.5, 308.5]
+    #cv2.rectangle(img, (483, 377), (599, 470), (0, 255, 0), 2)
+    slot3 = [[483, 377], [599, 377], [483, 470], [599, 470]]#slot3
+    slot3_mP = [541, 423.5]
+    #cv2.rectangle(img, (645, 377), (792, 474), (0, 255, 0), 2)
+    slot4 = [[645, 377], [792, 377], [645, 474], [792, 474]]#slot4
+    slot4_mP = [718.5, 425.5]
+    #cv2.rectangle(img, (825, 382), (949, 475), (0, 255, 0), 2)
+    slot5 = [[825, 382], [949, 382], [825, 475], [949, 475]]#slot5
+    slot5_mP = [887, 428.5]
+    slots = [slot0, slot1, slot2, slot3, slot4, slot5]
+    midpointsSlots = [slot0_mP, slot1_mP, slot2_mP, slot3_mP, slot4_mP, slot5_mP]
+    imgConts, conts = MiniUtils.getContours(img, depth, filter=4, showCanny=True)
+    midpointsConts = MiniUtils.midPoints(conts)
+    '''frame_depth = np.clip(capture.depth, 0, 2 ** 10 - 1)
+    frame_depth >>= 2
+    num_fingers, img_draw = handEstimator(frame_depth)
+    if len(conts) > 0:
+        switcher = {
+            0: goalPosition[0] = [midpointsSlots[0], midpointsConts.pop(0)]
+            1: goalPosition[1] = [midpointsSlots[1], midpointsConts.pop(0)]
+            2: goalPosition[2] = [midpointsSlots[2], midpointsConts.pop(0)]
+            3: goalPosition[3] = [midpointsSlots[3], midpointsConts.pop(0)]
+            4: goalPosition[4] = [midpointsSlots[4], midpointsConts.pop(0)]
+            5: goalPosition[5] = [midpointsSlots[5], midpointsConts.pop(0)]
+            }
+        switcher.get(num_fingers, 'Error')
+        
+    '''
+
+    #print(len(conts))
     while 1:
-        capture = k4a.get_capture()
-        if np.any(capture.color) and np.any(capture.depth):
-            depth_image = np.asanyarray(capture.transformed_depth)
-            color_image = np.asanyarray(capture.color)
-            depth_colormap = colorize(depth_image, (None, 5000), cv2.COLORMAP_HSV)
-            cv2.imwrite('colorimg.bmp', color_image)
-            cv2.imwrite('depth.bmp', depth_image)
-            cv2.imwrite('coloredDepth.png', depth_colormap)
+
+        if len(conts) != 0:
+            for obj in conts:
+                cv2.polylines(imgConts, [obj[2]], True, (0, 255, 0), 2)
+        #cv2.imshow('contss', imgConts)
+        for slot in slots:
+            cv2.rectangle(imgConts, (slot[0][0], slot[0][1]), (slot[3][0], slot[3][1]), (0, 255, 0), 2)
+        cv2.imshow('img', img)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            cv2.destroyAllWindows()
             break
-    k4a.stop()
+
 
 if __name__ == "__main__":
     main()
