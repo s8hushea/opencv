a48a03fa52edb21168f6d09713fdcf0cfde54bc7
hussein <s8hushea@stud.uni-saarland.de>
2021-03-09 14:25:50 +0100

    Cz

--- viewer.py
+++ viewer.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import json
 import pyk4a
 from helpers import colorize
 from pyk4a import Config, PyK4A
@@ -9,9 +9,9 @@ from pyk4a import Config, PyK4A
 def main():
     k4a = PyK4A(
         Config(
-            color_resolution=pyk4a.ColorResolution.OFF,
+            color_resolution=pyk4a.ColorResolution.RES_720P,
             depth_mode=pyk4a.DepthMode.NFOV_UNBINNED,
-            synchronized_images_only=False,
+            synchronized_images_only=True,
         )
     )
     k4a.start()
@@ -21,13 +21,17 @@ def main():
     assert k4a.whitebalance == 4500
     k4a.whitebalance = 4510
     assert k4a.whitebalance == 4510
-
     while True:
         capture = k4a.get_capture()
         if np.any(capture.depth):
-            cv2.imshow("k4a", colorize(capture.depth, (None, 5000), cv2.COLORMAP_HSV))
-            key = cv2.waitKey(10)
-            if key != -1:
+            trns_depth = capture.transformed_depth
+            with open('depthvalues.json', 'w') as f:
+                json.dump(trns_depth.tolist(), f)
+            cv2.imshow('trns',colorize(trns_depth,(None,None),cv2.COLORMAP_HSV))
+            cv2.imwrite('color.bmp', capture.color)
+            cv2.imwrite('trDepth.bmp', trns_depth)
+            key = cv2.waitKey(1)
+            if key == ord('q'):
                 cv2.destroyAllWindows()
                 break
     k4a.stop()
