0b466298b15fcf1f876d20d7aa4743b5035eddce
hussein <s8hushea@stud.uni-saarland.de>
2021-03-12 13:10:53 +0100

    add

--- calc.py
+++ calc.py
@@ -4,14 +4,14 @@ import numpy as np
 
 camera_mat = np.array([
     [
-      1138.9787523799525,
+      1632.0872262376538,
       0.0,
-      631.1836941706819
+      644.318903284315
     ],
     [
       0.0,
-      1333.7658172355304,
-      314.83373824314583
+      1204.7068501030717,
+      359.7371945876647
     ],
     [
       0.0,
@@ -21,11 +21,12 @@ camera_mat = np.array([
   ])
 
 def calculatepixels2coord(pixel_x, pixel_y, depth_transformed):
-    C_z = depth_transformed[pixel_x][pixel_y]
-    print(C_z)
+    C_z = depth_transformed[pixel_y][pixel_x]
     u_p_v = np.array([[pixel_x], [pixel_y], [1.]])
     camera_mat_inv = np.linalg.inv(camera_mat)
     C_x_y_byZ = np.dot(camera_mat_inv, u_p_v)
-    print(C_x_y_byZ)
-    #C_x = (C_x_y_byZ[0][0])/C_z
-    #C_y = (C_x_y_byZ[1][0])/C_z
+    C_x = (C_x_y_byZ[0][0])*C_z
+    C_y = (C_x_y_byZ[1][0])*C_z
+    print(C_x)
+    print(C_y)
+    print(C_z)
--- viewer.py
+++ viewer.py
@@ -4,19 +4,19 @@ import json
 import pyk4a
 from helpers import colorize
 from pyk4a import Config, PyK4A
-
+import calc
 
 def main():
-    k4a = PyK4A(
+    '''k4a = PyK4A(
         Config(
             color_resolution=pyk4a.ColorResolution.RES_720P,
             depth_mode=pyk4a.DepthMode.NFOV_UNBINNED,
             synchronized_images_only=True,
         )
     )
-    k4a.start()
+    k4a.start()'''
 
-    # getters and setters directly get and set on device
+    '''# getters and setters directly get and set on device
     k4a.whitebalance = 4500
     assert k4a.whitebalance == 4500
     k4a.whitebalance = 4510
@@ -34,8 +34,10 @@ def main():
             if key == ord('q'):
                 cv2.destroyAllWindows()
                 break
-    k4a.stop()
-
+    k4a.stop()'''
+    with open('depthvalues.json') as f:
+        transformed_depth = json.load(f)
+    calc.calculatepixels2coord(712, 105, transformed_depth)
 
 if __name__ == "__main__":
     main()
